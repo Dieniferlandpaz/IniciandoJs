54cf89a107deb62f9e3abd9849d7faf330ca299b
Dieniferlandpaz <dienepaz@hotmail.com>
2023-04-05 22:05:16 -0300

    Iniciando Js

---
+++ script.js
@@ -0,0 +1,33 @@
+// meu primeiro comentário 
+
+/* hjjhjkjk
+klklkl
+*/
+console.log('Olá mundo');
+prompt ("Qual o seu nome?");
+
+
+// Conhecendo e trabalhando com variáveis 
+const diaDeNascimento = 17;
+let texto = "Meu dia de nascimento é: ";
+
+console.log(texto);
+console.log(diaDeNascimento);
+
+// Concatenando variáveis 
+
+console.log(texto + diaDeNascimento );
+console.log(texto, diaDeNascimento);
+console.log ("Meu dia de nascimento é: " , diaDeNascimento);
+
+// Exemplos:
+const primeiroNome = 'Dienifer';
+const segundoNome = "Land";
+let minhaIdade = 24;
+let souEstudante = true;
+console.log ("Meu primeiro nome é: " , primeiroNome , "Meu sobrenome é:" , segundoNome, "Minha idade é: " , minhaIdade , "Sou estudante: " , souEstudante );
+
+
+
+
+
